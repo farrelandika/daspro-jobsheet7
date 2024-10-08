ccb95c0c624130aabd4880e11a2d74998ba7f8ba
unknown <farrelandika14@gmail.com>
2024-10-09 04:43:18 +0800

    jobsheet 7 percobaan 1

---
+++ SiakadFor_09.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SiakadFor_09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa Ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+                
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+                
+            }
+        }
+        
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+
+
+
+
+        
+    }
+}
